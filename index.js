20b22bb444e3178ed089012714b26cd3b2752219
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-06-23 20:24:19 -0500

    added some express

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded);
+app.use(bodyParser.json);
+
+
+const http = require('http');
+const server = http.createServer(app);
+
+
+
 const pgPromise = require('pg-promise');
 
+
 app.use(express.static('public'));
+
+server.listen(5000, () => {
+  console.log('listening on *:5000')
+});
