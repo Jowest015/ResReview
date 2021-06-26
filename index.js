3b475b109a3aaf53af299b69cc317637973a5195
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-06-26 13:28:13 -0500

    update to css html and index

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
+const server = http.createServer(app);
 
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var es6Renderer = require('express-es6-template-engine');
+var pgPromise = require('pg-promise')({});
+var db = pgPromise({database:''});
+
+var app = express();
+
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
 
 app.use(bodyParser.urlencoded);
 app.use(bodyParser.json);
 
-const http = require('http');
-const server = http.createServer(app);
-
-
-
-const pgPromise = require('pg-promise');
-
-
+app.use("/public", express.static("public"))
 app.use(express.static('public'));
 
 server.listen(5000, () => {
   console.log('listening on *:5000')
 });
-
-app.use('')
