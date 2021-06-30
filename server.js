90a6a62712e056a6689d4d437ce6ba86180bef54
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-06-29 19:11:18 -0500

    added to server.js

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const http = require('http');
 const server = http.createServer(app);
 
+var es6Renderer = require('express-es6-template-engine');
 var express = require('express');
 var bodyParser = require('body-parser');
-var es6Renderer = require('express-es6-template-engine');
-var pgPromise = require('pg-promise')({});
-var db = pgPromise({database:''});
-
+var pgPromise = require('pg-promise')({
+  promiseLib: Promise
+});
 var app = express();
+
+var db = pgPromise({
+  database:'restaurant'
+});
 
 app.engine("html", es6Renderer);
 app.set("views", "templates");
 app.set("view engine", "html");
 
-app.use(bodyParser.urlencoded);
-app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use("/public", express.static("public"))
 app.use(express.static('public'));
+
+app.get('/', function(req, resp) {
+  resp.render('search_form.html');
+});
+
+app.get('/search', function(req, resp) {
+
+})
 
 server.listen(5000, () => {
   console.log('listening on *:5000')
